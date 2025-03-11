e82917ec39c4bc0e83e05068a763173314e38c35
Maddy4201 <sonimadhur77@gmail.com>
2025-03-11 19:04:54 +0530

    added more commands to run.bat file

--- run.bat
+++ run.bat
@@ -1,6 +1,12 @@
 @echo off
 cd /d %~dp0
 echo Running PyCharm Automation Tests...
+rem pytest -s -v .\test_cases\test_execute_login.py
+rem pytest -s -v .\test_cases\test_add_update.py
+rem pytest -s -v .\test_cases\test_add_product.py
+rem pytest -s -v .\test_cases\test_changeBusiness_city.py
+rem pytest -s -v .\test_cases\test_execute_buySubscription.py
+rem pytest -s -v .\test_cases\test_download_businessCard.py
 pytest -s -v -m "sanity" --html .\reports\report_chrome_one.html
 rem pytest -s -v -m "regression" --html .\reports\report_chrome.html
 pause

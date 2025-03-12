0aeae077546a887d82237aedd01e1f33d8a4f7d7
Maddy4201 <136068363+Maddy4201@users.noreply.github.com>
2025-03-12 19:20:24 +0530

    Update run.bat
    change in .bat file

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ echo Running PyCharm Automation Tests...
 pytest -s -v .\test_cases\test_execute_login.py
 rem pytest -s -v .\test_cases\test_add_update.py
 rem pytest -s -v .\test_cases\test_add_product.py
-pytest -s -v .\test_cases\test_changeBusiness_city.py
+rem pytest -s -v .\test_cases\test_changeBusiness_city.py
 rem pytest -s -v .\test_cases\test_execute_buySubscription.py
 rem pytest -s -v .\test_cases\test_download_businessCard.py
 rem pytest -s -v -m "sanity" --html .\reports\report_chrome_one.html

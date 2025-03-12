c8f1b90787ac8d2a74a79b10b0d56505e102fef0
Maddy4201 <sonimadhur77@gmail.com>
2025-03-12 19:55:29 +0530

    update .bat file to run with allure reports

--- run.bat
+++ run.bat
@@ -1,12 +1,25 @@
 @echo off
 cd /d %~dp0
 echo Running PyCharm Automation Tests...
-pytest -s -v .\test_cases\test_execute_login.py
-rem pytest -s -v .\test_cases\test_add_update.py
-rem pytest -s -v .\test_cases\test_add_product.py
-rem pytest -s -v .\test_cases\test_changeBusiness_city.py
-rem pytest -s -v .\test_cases\test_execute_buySubscription.py
-rem pytest -s -v .\test_cases\test_download_businessCard.py
-rem pytest -s -v -m "sanity" --html .\reports\report_chrome_one.html
-rem pytest -s -v -m "regression" --html .\reports\report_chrome.html
+
+:: Optional: Clear old Allure reports
+rmdir /s /q allure-results allure-report
+mkdir allure-results
+mkdir allure-report
+
+:: Run only the tests you want, comment out (rem) others
+
+pytest -s -v .\test_cases\test_execute_login.py --alluredir=allure-results
+rem pytest -s -v .\test_cases\test_add_update.py --alluredir=allure-results
+rem pytest -s -v .\test_cases\test_add_product.py --alluredir=allure-results
+rem pytest -s -v .\test_cases\test_changeBusiness_city.py --alluredir=allure-results
+rem pytest -s -v .\test_cases\test_execute_buySubscription.py --alluredir=allure-results
+rem pytest -s -v .\test_cases\test_download_businessCard.py --alluredir=allure-results
+
+:: Generate Allure report
+allure generate allure-results -o allure-report --clean
+
+:: Open Allure report in the browser (optional)
+allure open allure-report
+
 pause
